c5253753a54eb672b51fb21ea6d8ab83a71ba667
dandon1960 <danmwenda96@gmail.com>
2022-11-28 10:10:43 +0300

    new direction

--- htcs/src/pages/tests/navbar.test.js
+++
@@ -1,7 +0,0 @@
-import {render, screen, cleanup} from "@testing-library/user-event"
-import Navbar from "../Navbar"
-test('navbar test', () => {
-    render(<Navbar/>);
-    const navbarElement = screen.getByTestId('navbar-1');
-    expect(navbarElement).toBeInTheDocument();
-})
